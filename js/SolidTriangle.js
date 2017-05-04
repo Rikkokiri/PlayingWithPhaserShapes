81341ab0715d2fbce5ed9e8231e99d2e3d01b325
Rikkokiri <pilvi.rajala@gmail.com>
2017-05-04 18:34:52 +0300

    Created skeleton for SolidTriangle class

---
+++ js/SolidTriangle.js
@@ -0,0 +1,23 @@
+/**
+ * @author Rikkokiri
+ */
+
+
+/**
+ * Creates a new solid (filled with color) triangle.
+ *
+ *
+ */
+
+function SolidTriangle(){
+
+
+
+}
+
+
+SolidTriangle.prototype = {
+
+
+
+}
